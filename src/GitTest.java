0aa7a50cb2c50770346a78c25235b2d4a8ded2a8
Johnny <johnnyy5221@gmail.com>
2022-06-15 17:17:05 +0800

    hot-fix commit 1

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld1");
+        System.out.println("hot-fix test");
     }
 
 }
